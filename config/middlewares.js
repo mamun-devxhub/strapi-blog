8cafe2572899dda933011c291396d36bdd269ed6
mamun-devxhub <abdullah-mamun@devxhub.com>
2024-05-14 21:28:47 +0600

    implement-aws-s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,10 +1,11 @@
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  // "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
+  // "strapi::body"
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
@@ -12,6 +13,32 @@ module.exports = [
     name: "strapi::body",
     config: {
       jsonLimit: "10mb", // modify JSON body
+    },
+  },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
     },
   },
 ];
